187bca0fa932ace1796f90f61a0d78e9399baf78
Anson <ansfoong@gmail.com>
2022-09-06 07:46:21 -0700

    emit onGroupRecipientRemoved & onGroupRemoved event

--- src/gateway/gateway.ts
+++ src/gateway/gateway.ts
@@ -242,8 +242,20 @@ export class MessagingGateway
 
   @OnEvent('group.user.remove')
   handleGroupUserRemove(payload: RemoveGroupUserResponse) {
-    this.server
-      .to(`group-${payload.group.id}`)
-      .emit('onGroupRemovedUser', payload);
+    const { group, user } = payload;
+    const ROOM_NAME = `group-${payload.group.id}`;
+    const removedUserSocket = this.sessions.getUserSocket(payload.user.id);
+    console.log(payload);
+    console.log('Inside group.user.remove');
+    if (removedUserSocket) {
+      console.log('Emitting onGroupRemoved');
+      removedUserSocket.emit('onGroupRemoved', payload);
+      removedUserSocket.leave(ROOM_NAME);
+    }
+    this.server.to(ROOM_NAME).emit('onGroupRecipientRemoved', payload);
+    const onlineUsers = group.users
+      .map((user) => this.sessions.getUserSocket(user.id) && user)
+      .filter((user) => user);
+    // this.server.to(ROOM_NAME).emit('onlineGroupUsersReceived', { onlineUsers });
   }
 }
