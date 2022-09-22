fa3f793481f68725f66c7080bbc2850da604be1a
J4SON <jason@chatr.fr>
2022-09-22 14:30:47 +0200

    add ApiTags

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -9,17 +9,16 @@ import {
   Res,
   UseGuards,
 } from '@nestjs/common';
-import { Throttle } from '@nestjs/throttler';
+import * as swagger from '@nestjs/swagger';
 import { instanceToPlain } from 'class-transformer';
 import { Request, Response } from 'express';
 import { IUserService } from '../users/interfaces/user';
 import { Routes, Services } from '../utils/constants';
-import { AuthUser } from '../utils/decorators';
-import { User } from '../utils/typeorm';
 import { IAuthService } from './auth';
 import { CreateUserDto } from './dtos/CreateUser.dto';
 import { AuthenticatedGuard, LocalAuthGuard } from './utils/Guards';
 
+@swagger.ApiTags(Routes.AUTH)
 @Controller(Routes.AUTH)
 export class AuthController {
   constructor(
@@ -28,22 +27,34 @@ export class AuthController {
   ) {}
 
   @Post('register')
+  @swagger.ApiCreatedResponse({
+    description: 'User created',
+    type: CreateUserDto,
+  })
   async registerUser(@Body() createUserDto: CreateUserDto) {
     return instanceToPlain(await this.userService.createUser(createUserDto));
   }
 
   @UseGuards(LocalAuthGuard)
   @Post('login')
+  @swagger.ApiCreatedResponse({
+    description: 'User logged in',
+    type: CreateUserDto,
+  })
   login(@Res() res: Response) {
     return res.send(HttpStatus.OK);
   }
 
   @Get('status')
+  @swagger.ApiOkResponse()
   @UseGuards(AuthenticatedGuard)
   async status(@Req() req: Request, @Res() res: Response) {
     res.send(req.user);
   }
 
   @Post('logout')
-  logout() {}
+  @swagger.ApiOkResponse()
+  logout() {
+    return HttpStatus.OK;
+  }
 }
