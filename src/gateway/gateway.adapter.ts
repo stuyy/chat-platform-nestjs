9bd54091a5f6a1cc24dcb29797894f02db8d093e
Anson <ansfoong@gmail.com>
2022-10-02 22:01:25 -0700

    null check if passport and passport.user properties are undefined

--- src/gateway/gateway.adapter.ts
+++ src/gateway/gateway.adapter.ts
@@ -29,6 +29,9 @@ export class WebsocketAdapter extends IoAdapter {
       if (!signedCookie) return next(new Error('Error signing cookie'));
       const sessionDB = await sessionRepository.findOne({ id: signedCookie });
       if (!sessionDB) return next(new Error('No session found'));
+      const userFromJson = JSON.parse(sessionDB.json);
+      if (!userFromJson.passport || !userFromJson.passport.user)
+        return next(new Error('Passport or User object does not exist.'));
       const userDB = plainToInstance(
         User,
         JSON.parse(sessionDB.json).passport.user,
